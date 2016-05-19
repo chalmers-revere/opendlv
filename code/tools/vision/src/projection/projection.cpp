5d5631782147bf8aeb6d908eca4deac6c6d962da
olbender <olbender@users.noreply.github.com>
2016-05-20 00:20:05 +0200

    Fixed messages in tooös/vision/projection.

--- code/tools/vision/src/projection/projection.cpp
+++ code/tools/vision/src/projection/projection.cpp
@@ -474,11 +474,12 @@ void Projection::Save()
     //     Eigen::DontAlignCols, ", ", "\n");
     const static Eigen::IOFormat saveFormat(Eigen::StreamPrecision,
         Eigen::DontAlignCols, " ", " ", "", "", "", "");
-    struct stat st;
-    if (stat(m_path.c_str(), &st) == -1) {
-      system(("mkdir -p " + m_path).c_str());
+    
+    //struct stat st;
+    //if (stat(m_path.c_str(), &st) == -1) {
+      //system(("mkdir -p " + m_path).c_str());
       // std::cout<<"Created dir"<<std::endl;
-    }
+    //}
 
     std::ofstream file(m_path + "/" + projectionMatrixFileName );
     if(file.is_open()){
