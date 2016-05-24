2b55d8684b175ee9bdbd87b306a96265b97030b9
Johan Bergström <johan.bergstroem@gmail.com>
2016-05-24 22:49:35 +0200

    Fixed printouts in scene and v2vcam

--- code/system/application/knowledge/src/linguistics/v2vcam/v2vcam.cpp
+++ code/system/application/knowledge/src/linguistics/v2vcam/v2vcam.cpp
@@ -220,7 +220,7 @@ odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode V2vCam::body()
       output += "Yaw rate confidence: " 
           + std::to_string(GetYawRateConfidence()) + "\n";
       output += "Vehicle role: " + std::to_string(GetVehicleRole()) + "\n";
-      std::cout << output;
+      //std::cout << output;
   }
   return odcore::data::dmcp::ModuleExitCodeMessage::OKAY;
 }
@@ -342,7 +342,7 @@ void V2vCam::ReadVoice(opendlv::sensation::Voice const &a_voice)
     output += "Yaw rate confidence: " 
         + std::to_string(yawRateConfidence) + "\n";
     output += "Vehicle role: " + std::to_string(vehicleRole) + "\n";
-    // std::cout << output;
+    std::cout << output;
 
     m_receiveLog << std::to_string(messageId) +
         + "," + std::to_string(stationId) +
@@ -368,6 +368,8 @@ void V2vCam::ReadVoice(opendlv::sensation::Voice const &a_voice)
         + "," + std::to_string(vehicleRole);
         m_receiveLog << std::endl; 
 
+
+    //This is where the objects are constructed whihc are to be sent out
     opendlv::data::environment::WGS84Coordinate gpsReference;
 
     gpsReference = opendlv::data::environment::WGS84Coordinate(
