ca178647e01eb384f90b1308c20696d7d9e267e8
Christian Berger <christian.berger@gu.se>
2016-06-08 12:03:53 -0700

    * Changed build script
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -10,20 +10,20 @@ if [ "$1" == "FRESH" ]; then
     mkdir install
 
     cd build
-    cmake -D CMAKE_INSTALL_PREFIX=../../docker/fh16/opendlv ../..
+    cmake -D CMAKE_INSTALL_PREFIX=../install ../..
     make -j3
     cd ..
 
     mkdir build.system
     cd build.system
-    cmake -DEIGEN3_INCLUDE_DIR=../../thirdparty/eigen3 -DKALMAN_INCLUDE_DIR=../../thirdparty/kalman/include -DTINYCNN_INCLUDE_DIR=../../thirdparty/tiny-cnn -DCMAKE_INSTALL_PREFIX=../../docker/fh16/opendlv ../../code/system
+    cmake -DEIGEN3_INCLUDE_DIR=../../thirdparty/eigen3 -DKALMAN_INCLUDE_DIR=../../thirdparty/kalman/include -DTINYCNN_INCLUDE_DIR=../../thirdparty/tiny-cnn -DCMAKE_INSTALL_PREFIX=../install ../../code/system
     make -j3
     make install
     cd ..
 fi
 
 if [ "$1" == "INCREMENTAL" ]; then
-    if [ -d build.from.docker -a -d build.from.docker/build -a -d build.from.docker/../docker/fh16/opendlv -a -d build.from.docker/build.system ]; then
+    if [ -d build.from.docker -a -d build.from.docker/build -a -d build.from.docker/install -a -d build.from.docker/build.system ]; then
         cd build.from.docker
         cd build.system
         make -j3
