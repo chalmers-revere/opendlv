89c0899da5ee1a714a88c9c0237dd89d60088b75
Björnborg Nguyen <nguyenb@student.chalmers.se>
2016-10-03 18:33:27 +0200

    Added warping if needed, added region of interest

--- code/system/application/perception/src/detectlane/detectlane.cpp
+++ code/system/application/perception/src/detectlane/detectlane.cpp
@@ -103,9 +103,15 @@ void DetectLane::nextContainer(odcore::data::Container &a_c)
   m_image = tmpImage.clone();
   cvReleaseImage(&myIplImage);
 
+  cv::Rect myROI(0, 450, 1000, 270);
+  m_image = m_image(myROI);
+
+
+
   cv::Mat cannyImg;
   cv::Mat intenImg;
   cv::Mat color_dst;
+
 
   cv::Canny(m_image, cannyImg, m_cannyThreshold, m_cannyThreshold*3, 3);
   //medianBlur(src,src,3);
@@ -141,19 +147,73 @@ void DetectLane::nextContainer(odcore::data::Container &a_c)
     cv::Point pt2(cvRound(x0 - 2000*(-b)),
                cvRound(y0 - 2000*(a)));
 
-    line(m_image, pt1, pt2, cv::Scalar(0,0,255), 1, 1 );
+    cv::line(cannyImg, pt1, pt2, cv::Scalar(255,255,255), 1, 1 );
   }
-  const int32_t windowWidth = 640;
-  const int32_t windowHeight = 480;
-  cv::Mat display;
-  cv::Mat displa2;
-  cv::resize(cannyImg, display, cv::Size(windowWidth, windowHeight), 0, 0,
-    cv::INTER_CUBIC);
-  cv::imshow("FOE", display);
-  cv::resize(m_image, displa2, cv::Size(windowWidth, windowHeight), 0, 0,
-    cv::INTER_CUBIC);
-  cv::imshow("FOE2", displa2);
+
+  cv::HoughLines(intenImg, detectedLines, 1, 3.14f/180, m_houghThreshold );
+  // std::cout << "Unfiltered: "<< detectedLines.size() << std::endl;
+  test.clear();
+  for(uint16_t i = 0; i != detectedLines.size(); i++){
+    if(angleTresh > detectedLines[i][1] || 3.14f-angleTresh < detectedLines[i][1]){
+      test.push_back(detectedLines[i]);
+    }
+  }
+  detectedLines = test;
+  // std::cout << "Filtered: "<< detectedLines.size() << std::endl;
+
+
+  for( size_t i = 0; i < detectedLines.size(); i++ ) {
+    float rho = detectedLines[i][0];
+    float theta = detectedLines[i][1];
+    float a = cos(theta), b = sin(theta);
+    float x0 = a*rho, y0 = b*rho;
+    cv::Point pt1(cvRound(x0 + 2000*(-b)),
+               cvRound(y0 + 2000*(a)));
+    cv::Point pt2(cvRound(x0 - 2000*(-b)),
+               cvRound(y0 - 2000*(a)));
+
+    cv::line(intenImg, pt1, pt2, cv::Scalar(255,255,255), 1, 1 );
+  }
+
+
+  // // Input Quadilateral or Image plane coordinates
+  // cv::Point2f inputQuad[4] = {cv::Point2f(350,470),cv::Point2f(750,470),cv::Point2f(2180,720),cv::Point2f(-700,720)};
+  // // Output Quadilateral or World plane coordinates
+  // cv::Point2f outputQuad[4] = {cv::Point2f(0,0),cv::Point2f(1080,0),cv::Point2f(1080,720),cv::Point2f(0,720)}; 
+  // // std::cout << inputQuad << outputQuad << std::endl;
+  // cv::Mat lambda = cv::getPerspectiveTransform(inputQuad, outputQuad);
+  // cv::Mat warpedImg = m_image.clone();
+  // cv::warpPerspective(m_image, warpedImg, lambda, warpedImg.size());
+
+
+  // cv::Point polypoint[4];
+  // for(int i=0;i<4;i++){
+  //     polypoint[i]= inputQuad[i];
+  // }
+  // const cv::Point* countours[1]={polypoint};
+  // int v = 4;
+  // const int* constCont = &v; 
+  
+
+  // cv::polylines(m_image, countours, constCont, 4, 1, cv::Scalar(0,255,0), 4, 8, 0);
+
+  // const int32_t windowWidth = 640;
+  // const int32_t windowHeight = 480;
+  // cv::Mat display;
+  // cv::Mat displa2;
+  // cv::Mat displa3;
+  // cv::resize(cannyImg, display, cv::Size(windowWidth, windowHeight), 0, 0,
+    // cv::INTER_CUBIC);
+  cv::imshow("FOE", cannyImg);
+  // cv::resize(m_image, displa2, cv::Size(windowWidth, windowHeight), 0, 0,
+    // cv::INTER_CUBIC);
+  cv::imshow("FOE2", m_image);
+  // cv::resize(warpedImg, displa3, cv::Size(windowWidth, windowHeight), 0, 0,
+  //   cv::INTER_CUBIC);
+  cv::imshow("FOE3", intenImg);
+
   cv::waitKey(1);
+  // warpedImg.release();
 
 }
 
