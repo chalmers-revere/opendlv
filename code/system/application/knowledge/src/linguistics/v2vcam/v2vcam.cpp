0a7e033f92e3e942fe403ae0f270ad03f2033523
Julian Scholle <julian.scholle@googlemail.com>
2016-10-16 20:23:36 +0200

    add receiving unfiltered GPS data

--- code/system/application/knowledge/src/linguistics/v2vcam/v2vcam.cpp
+++ code/system/application/knowledge/src/linguistics/v2vcam/v2vcam.cpp
@@ -287,6 +287,11 @@ void V2vCam::nextContainer(odcore::data::Container &a_c)
        m_latitude = temp.getLatitude();
        m_longitude = temp.getLongitude();
   }
+  else if(a_c.getDataType() == opendlv::proxy::GpsReading::ID()) {
+       opendlv::data::environment::WGS84Coordinate temp = a_c.getData<opendlv::data::environment::WGS84Coordinate>();
+       m_latitude = temp.getLatitude();
+       m_longitude = temp.getLongitude();
+  }
 }
 
 void V2vCam::ReadInsight(opendlv::knowledge::Insight const &a_insight)
