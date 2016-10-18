a862b7b28e7027f3ece2f07d66c7c350c9c88564
olbender <olbender@users.noreply.github.com>
2016-10-18 12:44:33 +0200

    Fix.

--- code/system/application/knowledge/src/gcdc16/rule/intersectionleft/intersectionleft.cpp
+++ code/system/application/knowledge/src/gcdc16/rule/intersectionleft/intersectionleft.cpp
@@ -104,8 +104,8 @@ void IntersectionLeft::ActOnMio(std::vector<opendlv::perception::Object> &a_list
         
         std::cout << "The MIO was found in azimuth " << azimuth << " at a distance of " << distance << " m." << std::endl;
 
-        float longitudinal = cos(azimuth) * distance;
-        float lateral = sin(azimuth) * distance;
+        float longitudinal = sin(azimuth) * distance;
+        float lateral = cos(azimuth) * distance;
 
         std::cout << "  - Longitudinal: " << longitudinal << std::endl;
         std::cout << "  - Lateral: " << lateral << std::endl;
