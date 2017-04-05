ed94f7680393f94270a21a24a8b220e5c643c561
Christian Berger <christian.berger@gu.se>
2017-04-05 21:33:11 +0200

    * Container flow extraction script
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- scripts/extractDataflow.sh
+++ scripts/extractDataflow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # OpenDaVINCI - Portable middleware for distributed components.
-# Copyright (C) 2016  Christian Berger
+# Copyright (C) 2017  Christian Berger
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,31 +16,31 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-cat <<EOF >../extractSending.awk
-BEGIN { RS = "--" ; FS = "\n" }
+:>containers.io
+FILES_SENDING_CONTAINERS=$(grep "getConference().send(" -r .|cut -f1 -d":"|sort|uniq)
+for i in $FILES_SENDING_CONTAINERS; do
+    :>`basename $i`.lst
+    for j in $(grep "getConference().send(" $i | sed -e 's/.send(/%/g'|cut -f2 -d"%"|cut -f1 -d")"); do
+        #echo "File: $i, container: $j"
+        CONTAINER_ARG1=$(cat $i | grep "Container $j" | grep -v "//" | cut -f1 -d"," | cut -f2 -d"(" | cut -f1 -d"[" | cut -f1 -d")")
+        #echo "Type name $CONTAINER_ARG1"
+        cat $i | grep -G -B50 "Container $j($CONTAINER_ARG1" > out
+        MESSAGE=$(tac out | grep -v "Container" | grep -v "//" | grep -v "\[" | grep -v "push_back" | grep -v "<<" | grep ".*\s.*" | grep -G " $CONTAINER_ARG1(" -m 1 | sed -e 's/^[[:space:]]*//')
+        if [ "$MESSAGE" != "" -a "$(echo $MESSAGE | wc -w)" != "1" ]; then
+            #echo "Message type = $MESSAGE"
+            T=$(echo $MESSAGE | cut -f1 -d" ")
+            echo "$i;sends;$T" >> `basename $i`.lst
+        fi
+        rm -f out
+    done
+    cat `basename $i`.lst | sort | uniq >> containers.io && rm -f `basename $i`.lst
+done
 
-{
-    # Skip first $1
-    split(\$2, FIRSTLINE, " ")
-    SOURCE=gensub(/\-/, "", "g", FIRSTLINE[1])
-    TYPE=FIRSTLINE[2]
-    split(FIRSTLINE[3], NAME, "(")
+# Find lines where the content of a Container is queried. Such lines are usually
+# found when a Container has been received.
+#grep ".*getDataType().*==.*::ID()" -r . | sed -e 's/\:.*\=\=//g' | sed -e 's/.{//g' > containers.received
+grep ".*getDataType().*==.*::ID()" -r . | sed -e 's/\:.*\=\=//g' | sed -e 's/.{//g' > out
+cat out | rev | cut -f3- -d":" | rev | sed -e 's/ /;receives;/' >> containers.io && rm -f out
 
-    split(\$3, SECONDLINE, " ")
-    CONTAINER1=SECONDLINE[3]
-    CONTAINER2=SECONDLINE[4]
-    CONTAINER3=SECONDLINE[5]
-    CONTAINER4=SECONDLINE[6]
-
-    if ( (SOURCE != "" ) && (TYPE != "") && (NAME[1] != "") && (CONTAINER1 != "") ) {
-        print SOURCE, TYPE, NAME[1], CONTAINER1, CONTAINER2, CONTAINER3, CONTAINER4
-    }
-}
-EOF
-
-# Get data about sending Containers.
-grep "getConference().send(" -B2 -r . | gawk -f ../extractSending.awk > containers.sent && rm -f ../extractSending.awk
-
-grep "==.*::ID()" -r . | sed -e 's/\:.*\=\=//g' | sed -e 's/.{//g' > containers.received
-
+cat containers.io | sort
 
