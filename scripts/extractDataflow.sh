279c78b8fcd3b2fd1365f67b0915ae5d29c37fe4
Christian Berger <christian.berger@gu.se>
2017-08-06 23:15:28 +0200

    * Updated extractDataFlow.sh to report everything on OpenDLV.core
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- scripts/extractDataflow.sh
+++ scripts/extractDataflow.sh
@@ -17,21 +17,58 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 :>containers.io
-FILES_SENDING_CONTAINERS=$(grep "getConference().send(" -r .|cut -f1 -d":"|sort|uniq)
+FILES_SENDING_CONTAINERS=$(grep -E "(getConference\(\)|onference)\.send\(" -r .|cut -f1 -d":"|sort|uniq)
 for i in $FILES_SENDING_CONTAINERS; do
     :>`basename $i`.lst
-    for j in $(grep "getConference().send(" $i | sed -e 's/.send(/%/g'|cut -f2 -d"%"|cut -f1 -d")"); do
-        #echo "File: $i, container: $j"
+    for j in $(grep -E "(getConference\(\)|onference)\.send\(" $i | sed -e 's/.send(/%/g'|cut -f2 -d"%"|cut -f1 -d")"); do
+#        echo "File: $i, container: $j"
         CONTAINER_ARG1=$(cat $i | grep "Container $j" | grep -v "//" | cut -f1 -d"," | cut -f2 -d"(" | cut -f1 -d"[" | cut -f1 -d")")
-        #echo "Type name $CONTAINER_ARG1"
-        cat $i | grep -G -B50 "Container $j($CONTAINER_ARG1" > out
-        MESSAGE=$(tac out | grep -v "Container" | grep -v "//" | grep -v "\[" | grep -v "push_back" | grep -v "<<" | grep ".*\s.*" | grep -G " $CONTAINER_ARG1(" -m 1 | sed -e 's/^[[:space:]]*//')
-        if [ "$MESSAGE" != "" -a "$(echo $MESSAGE | wc -w)" != "1" ]; then
-            #echo "Message type = $MESSAGE"
-            T=$(echo $MESSAGE | cut -f1 -d" ")
-            echo "$i;sends;$T" >> `basename $i`.lst
-        fi
-        rm -f out
+        for k in $CONTAINER_ARG1; do
+#            echo "Found type name $k"
+            # Find type declaration as a variable previously defined in scope.
+            cat $i | grep -G -B150 "Container $j($k" > out
+            # Search for constructor instantiation.
+            TYPE_PROCESSED=0
+            if [ "$TYPE_PROCESSED" == "0" -a "${k:0:2}" == "m_" ]; then
+                HEADER=$(echo `dirname $i`/../include/`basename $i|cut -f1 -d"."`.h)
+#                echo "$k is probably defined in header file: $HEADER"
+                if [ -f $HEADER ]; then
+                    MESSAGE=$(cat $HEADER | grep ".*\s.*" | grep -G "$k" -m 1 | sed -e 's/^[[:space:]]*//')
+                    if [ "$TYPE_PROCESSED" == "0" -a "$MESSAGE" != "" -a "$(echo $MESSAGE | wc -w)" != "1" ]; then
+#                        echo "Message type = $MESSAGE"
+                        T=$(echo $MESSAGE | cut -f1 -d" ")
+                        echo "$i;sends;$T" >> `basename $i`.lst
+                    fi
+                else
+                    echo "$k is probably defined in header file $HEADER but the file was not found."
+                fi
+                TYPE_PROCESSED=1
+            fi
+            MESSAGE=$(tac out | grep -v "Container" | grep -v "//" | grep -v "\[" | grep -v "push_back" | grep -v "return" | grep -v "<<" | grep ".*\s.*" | grep -G " $k(" -m 1 | sed -e 's/^[[:space:]]*//')
+            if [ "$TYPE_PROCESSED" == "0" -a "$MESSAGE" != "" -a "$(echo $MESSAGE | wc -w)" != "1" ]; then
+#                echo "Message type = $MESSAGE"
+                T=$(echo $MESSAGE | cut -f1 -d" ")
+                echo "$i;sends;$T" >> `basename $i`.lst
+                TYPE_PROCESSED=1
+            fi
+            # Search for constructor-less instantiation.
+            MESSAGE=$(tac out | grep -v "Container" | grep -v "//" | grep -v "\[" | grep -v "push_back" | grep -v "return" | grep -v "<<" | grep ".*\s.*" | grep -G " $k;" -m 1 | sed -e 's/^[[:space:]]*//')
+            if [ "$TYPE_PROCESSED" == "0" -a "$MESSAGE" != "" -a "$(echo $MESSAGE | wc -w)" != "1" ]; then
+#                echo "Message type = $MESSAGE"
+                T=$(echo $MESSAGE | cut -f1 -d" ")
+                echo "$i;sends;$T" >> `basename $i`.lst
+                TYPE_PROCESSED=1
+            fi
+            # Search for assignment-based instantiation.
+            MESSAGE=$(tac out | grep -v "Container" | grep -v "//" | grep -v "\[" | grep -v "push_back" | grep -v "return" | grep -v "<<" | grep ".*\s.*" | grep -G " $k = " -m 1 | sed -e 's/^[[:space:]]*//')
+            if [ "$TYPE_PROCESSED" == "0" -a "$MESSAGE" != "" -a "$(echo $MESSAGE | wc -w)" != "1" ]; then
+#                echo "Message type = $MESSAGE"
+                T=$(echo $MESSAGE | cut -f1 -d" ")
+                echo "$i;sends;$T" >> `basename $i`.lst
+                TYPE_PROCESSED=1
+            fi
+            rm -f out
+        done
     done
     cat `basename $i`.lst | sort | uniq >> containers.io && rm -f `basename $i`.lst
 done
