1ff1d5a506536f9cea0cd675197e69e84f5e25a7
martin <martin9421@gmail.com>
2018-01-26 14:45:03 +0100

    Removed code from selflocalization.cpp

--- code/logic/sensation/selflocalization/src/selflocalization.cpp
+++ code/logic/sensation/selflocalization/src/selflocalization.cpp
@@ -49,63 +49,13 @@ Selflocalization::Selflocalization(int32_t const &a_argc, char **a_argv)
 Selflocalization::~Selflocalization()
 {
 }
-
-void Selflocalization::nextContainer(odcore::data::Container &a_c)
+/*
+*Takes data from conference, in our case image?
+*/
+void Selflocalization::nextContainer(odcore::data::Container &a_c) 
 {
-  // cout << "Received container of type " << c.getDataType() 
-  //     << " sent at " << c.getSentTimeStamp().getYYYYMMDD_HHMMSSms() 
-  //     <<" received at " << c.getReceivedTimeStamp().getYYYYMMDD_HHMMSSms()
-  //     << endl;
-  if(a_c.getDataType() != opendlv::proxy::V2vReading::ID()){
-    return;
-  }
-
-  // std::cout << "Received a message of type ."
-  opendlv::proxy::V2vReading v2vReading = 
-      a_c.getData<opendlv::proxy::V2vReading>();
-  std::string dataString = v2vReading.getData();
-  std::vector<unsigned char> data(dataString.begin(), dataString.end());
-  
-  // string value = message.getValue();
-  // vector<string> tokens = odcore::strings::StringToolbox::split(value, '|');
 
 
-  // std::cout << std::to_string(static_cast<unsigned char>(*data.begin())) 
-  //     << std::endl;
-  // std::vector<unsigned char> const bytes = data;
-  // std::stringstream ss;
-  // for (uint i = 0; i < bytes.size(); i++) {
-  //     ss << std::to_string(bytes.at(i));
-  //     ss << "|";
-  // }
-  // std::cout<<ss.str()<<std::endl;
-  // std::cout<<value<<std::endl;
-
-  unsigned char v2vMsgId = data.at(0);
-  // std::cout <<  std::to_string(v2vMsgId)<<std::endl;
-  std::string v2vMsgType;
-  switch(v2vMsgId)
-  {
-    case 1:
-      v2vMsgType = "denm";
-      // std::cout << "Sent denm." << std::endl;
-    break;
-    case 2:
-      v2vMsgType = "cam";
-      // std::cout << "Sent cam." << std::endl;
-    break;
-    case 10:
-      v2vMsgType = "iclcm";
-      // std::cout << "Sent iclcm." << std::endl;
-    break;
-    default:
-      std::cout << "Received invalid message ID.";
-  }
-  if(!v2vMsgType.empty())
-  {
-    // std::cout<<"Sorted and sending to next layer.\n";
-    opendlv::sensation::Voice nextMessage(v2vMsgType, v2vReading.getSize(), 
-        dataString);
     odcore::data::Container container(nextMessage);
     getConference().send(container);
   }
