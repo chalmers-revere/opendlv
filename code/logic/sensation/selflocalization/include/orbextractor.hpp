06eb7c354f5005a6af095295802b1e41ea8e228d
martin <martin9421@gmail.com>
2018-02-15 18:35:00 +0100

    Completed header file and constructor

--- code/logic/sensation/selflocalization/include/orbextractor.hpp
+++ code/logic/sensation/selflocalization/include/orbextractor.hpp
@@ -16,9 +16,26 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  */
+/*Maybe needed
+class ExtractorNode
+{
+public:
+    ExtractorNode():bNoMore(false){}
+
+    void DivideNode(ExtractorNode &n1, ExtractorNode &n2, ExtractorNode &n3, ExtractorNode &n4);
+
+    std::vector<cv::KeyPoint> vKeys;
+    cv::Point2i UL, UR, BL, BR;
+    std::list<ExtractorNode>::iterator lit;
+    bool bNoMore;
+};*/
 
 #ifndef OPENDLV_ORBEXTRACTOR_HPP
 #define OPENDLV_ORBEXTRACTOR_HPP
+
+#include<vector>
+#include<list>
+#include<opencv/cv.h>
 
 namespace opendlv {
 namespace logic {
@@ -26,8 +43,38 @@ namespace sensation {
 
 class OrbExtractor {
  public:
-  OrbExtractor();
+  OrbExtractor(int nFeatures, float scaleFactor, int nLevels, int initialFastTh, int minFastTh);
   ~OrbExtractor();
+  /*Called by other classes to get keypoints and descriptors*/
+  void ExtractFeatures(cv::InputArray image,std::vector<cv::KeyPoint>& keypoints,cv::OutputArray descriptors);
+
+  std::vector<cv::Mat> m_vImagePyramid;
+ private:
+   void ComputePyramid(cv::Mat image);
+   
+   void ComputeKeyPointsOctTree(std::vector<std::vector<cv::KeyPoint> >& allKeypoints);    
+   
+   std::vector<cv::KeyPoint> DistributeOctTree(const std::vector<cv::KeyPoint>& vToDistributeKeys, const int &minX,
+                                           const int &maxX, const int &minY, const int &maxY, const int &nFeatures, const int &level);
+
+   void ComputeKeyPointsOld(std::vector<std::vector<cv::KeyPoint> >& allKeypoints);
+   
+
+   std::vector<cv::Point> m_vPattern;
+
+   int m_nFeatures;
+   double m_scaleFactor;
+   int m_nLevels;
+   int m_initialFastTh;
+   int m_minFastTh;
+
+   std::vector<int> m_nFeaturesPerLevel;
+   std::vector<int> m_umax;
+
+   std::vector<float> m_vScaleFactor;
+   std::vector<float> m_vInvScaleFactor;    
+   std::vector<float> m_vLevelSigma2;
+   std::vector<float> m_vInvLevelSigma2;
 };
 
 } // namespace sensation
--- code/logic/sensation/selflocalization/src/orbextractor.cpp
+++ code/logic/sensation/selflocalization/src/orbextractor.cpp
@@ -22,9 +22,77 @@
 namespace opendlv {
 namespace logic {
 namespace sensation {
-OrbExtractor::OrbExtractor()
-{
-}
+OrbExtractor::OrbExtractor(int nFeatures, float scaleFactor, int nLevels, int initialFastTh, int minFastTh):
+		m_vImagePyramid()
+	, m_vPattern()
+	,	m_nFeatures(nFeatures)
+	, m_scaleFactor(scaleFactor)
+	, m_nLevels(nLevels)
+	, m_initialFastTh(initialFastTh)
+	, m_minFastTh(minFastTh)
+	, m_nFeaturesPerLevel()
+	, m_umax()
+	, m_vScaleFactor()
+	, m_vInvScaleFactor()
+	, m_vLevelSigma2()
+	, m_vInvLevelSigma2()
+	{
+		m_vScaleFactor.resize(nLevels);
+    m_vLevelSigma2.resize(nLevels);
+    m_vScaleFactor[0]=1.0f;
+    m_vLevelSigma2[0]=1.0f;
+    for(int i=1; i<nLevels; i++)
+    {
+        m_vScaleFactor[i]=m_vScaleFactor[i-1]*scaleFactor;
+        m_vLevelSigma2[i]=m_vScaleFactor[i]*m_vScaleFactor[i];
+    }
+
+    m_vInvScaleFactor.resize(nLevels);
+    m_vInvLevelSigma2.resize(nLevels);
+    for(int i=0; i<nLevels; i++)
+    {
+        m_vInvScaleFactor[i]=1.0f/m_vScaleFactor[i];
+        m_vInvLevelSigma2[i]=1.0f/m_vLevelSigma2[i];
+    }
+
+    m_vImagePyramid.resize(nLevels);
+
+    m_nFeaturesPerLevel.resize(nLevels);
+    float factor = 1.0f / scaleFactor;
+    float nDesiredFeaturesPerScale = nFeatures*(1 - factor)/(1 - (float)pow((double)factor, (double)nLevels));
+
+    int sumFeatures = 0;
+    for( int level = 0; level < nLevels-1; level++ )
+    {
+        m_nFeaturesPerLevel[level] = cvRound(nDesiredFeaturesPerScale);
+        sumFeatures += m_nFeaturesPerLevel[level];
+        nDesiredFeaturesPerScale *= factor;
+    }
+    m_nFeaturesPerLevel[nLevels-1] = std::max(nFeatures - sumFeatures, 0);
+
+    //const int npoints = 512;
+    //const Point* pattern0 = (const Point*)bit_pattern_31_;
+    //std::copy(pattern0, pattern0 + npoints, std::back_inserter(pattern));
+
+    //This is for orientation
+    // pre-compute the end of a row in a circular patch
+    /*m_umax.resize(HALF_PATCH_SIZE + 1);
+
+    int v, v0, vmax = cvFloor(HALF_PATCH_SIZE * sqrt(2.f) / 2 + 1);
+    int vmin = cvCeil(HALF_PATCH_SIZE * sqrt(2.f) / 2);
+    const double hp2 = HALF_PATCH_SIZE*HALF_PATCH_SIZE;
+    for (v = 0; v <= vmax; ++v)
+        m_umax[v] = cvRound(sqrt(hp2 - v * v));
+
+    // Make sure we are symmetric
+    for (v = HALF_PATCH_SIZE, v0 = 0; v >= vmin; --v)
+    {
+        while (m_umax[v0] == m_umax[v0 + 1])
+            ++v0;
+        m_umax[v] = v0;
+        ++v0;
+		}*/
+	}
 OrbExtractor::~OrbExtractor()
 {
 }
