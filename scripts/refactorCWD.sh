9e642722582efa9e07f9e7db548b421073fc240d
Christian Berger <christian.berger@gu.se>
2018-02-24 18:04:17 +0100

    * Removing existing stuff from OpenDLV
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- scripts/refactorCWD.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in `find . -name \*.cpp -o -name \*.hpp`; do 
-  clang-format $file > /tmp/formatted_file;
-  cat /tmp/formatted_file > $file;
-done
-
-rm /tmp/formatted_file;
--- thirdparty/kalman/cmake/UseKalman.cmake
+++
@@ -1,5 +0,0 @@
-#                                               -*- cmake -*-
-#
-#  UseKalman.cmake
-
-include_directories(${KALMAN_INCLUDE_DIR})
