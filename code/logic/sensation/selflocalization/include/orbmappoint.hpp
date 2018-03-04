4ec811fc70af60ed5810ca22047214bf9e5eb313
Pontus Pohl <pontus.pohl@gmail.com>
2018-03-04 16:04:48 +0100

    Add stub of orbmappoint #156

--- code/logic/sensation/selflocalization/include/orbmappoint.hpp
+++ code/logic/sensation/selflocalization/include/orbmappoint.hpp
@@ -33,14 +33,113 @@ class OrbFrame;
 class OrbMapPoint
 {
 public:
-    OrbMapPoint();
+    OrbMapPoint(const cv::Mat &Position, std::shared_ptr<OrbFrame> refenceKeyFrame, std::shared_ptr<OrbMap> map);
+    OrbMapPoint((const cv::Mat &Position, std::shared_ptr<OrbFrame> refenceKeyFrame, std::shared_ptr<OrbMap> map, const int &keyPointIndex)
     ~OrbMapPoint();
 
     std::map<std::shared_ptr<OrbFrame>,size_t> GetObservations();
 
-    bool IsBad();
+    void SetWorldPos(const cv::Mat &Pos);
+    cv::Mat GetWorldPos();
 
-    long unsigned int Id = 0;
+    cv::Mat GetNormal();
+    KeyFrame* GetReferenceKeyFrame();
+    int Observations();
+    int getSequenceId();
+    void AddObservation(KeyFrame* pKF,size_t idx);
+    void EraseObservation(KeyFrame* pKF);
+
+    int GetIndexInKeyFrame(KeyFrame* pKF);
+    bool IsInKeyFrame(KeyFrame* pKF);
+
+    void SetBadFlag();
+    bool isBad();
+
+    void Replace(MapPoint* pMP);    
+    MapPoint* GetReplaced();
+
+    void IncreaseVisible(int n=1);
+    void IncreaseFound(int n=1);
+    float GetFoundRatio();
+    inline int GetFound(){
+        return mnFound;
+    }
+
+    void ComputeDistinctiveDescriptors();
+
+    cv::Mat GetDescriptor();
+
+    void UpdateNormalAndDepth();
+
+    float GetMinDistanceInvariance();
+    float GetMaxDistanceInvariance();
+    int PredictScale(const float &currentDist, KeyFrame*pKF);
+    int PredictScale(const float &currentDist, Frame* pF);
+    static long unsigned int nNextId;
+
+private:
+    long unsigned int SequenceId = 0;
+    long int mnFirstKFid;
+    long int mnFirstFrame;
+    int nObs;
+
+    // Variables used by the tracking
+    float mTrackProjX;
+    float mTrackProjY;
+    float mTrackProjXR;
+    bool mbTrackInView;
+    int mnTrackScaleLevel;
+    float mTrackViewCos;
+    long unsigned int mnTrackReferenceForFrame;
+    long unsigned int mnLastFrameSeen;
+
+    // Variables used by local mapping
+    long unsigned int mnBALocalForKF;
+    long unsigned int mnFuseCandidateForKF;
+
+    // Variables used by loop closing
+    long unsigned int mnLoopPointForKF;
+    long unsigned int mnCorrectedByKF;
+    long unsigned int mnCorrectedReference;    
+    cv::Mat mPosGBA;
+    long unsigned int mnBAGlobalForKF;
+
+
+    static std::mutex mGlobalMutex;
+
+    // mutexed below
+
+     // Position in absolute coordinates
+     cv::Mat m_WorldPosition;
+
+     // Keyframes observing the point and associated index in keyframe
+     std::map<KeyFrame*,size_t> mObservations;
+
+     // Mean viewing direction
+     cv::Mat mNormalVector;
+
+     // Best descriptor to fast matching
+     cv::Mat mDescriptor;
+
+     // Reference KeyFrame
+     KeyFrame* mpRefKF;
+
+     // Tracking counters
+     int mnVisible;
+     int mnFound;
+
+     // Bad flag (we do not currently erase MapPoint from memory)
+     bool mbBad;
+     MapPoint* mpReplaced;
+
+     // Scale invariance distances
+     float mfMinDistance;
+     float mfMaxDistance;
+
+     Map* mpMap;
+
+     std::mutex mMutexPos;
+     std::mutex mMutexFeatures;
 };
 
 }
